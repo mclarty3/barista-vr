ea560ca84ead2fcea932dbbb362d4ea62447bdb1
mclarty3 <mclarty3@gmail.com>
2021-09-25 02:56:38 -0400

    Fix null parent issue

--- Assets/Scripts/PortafilterChute.cs
+++ Assets/Scripts/PortafilterChute.cs
@@ -6,7 +6,7 @@ public class PortafilterChute : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        if (other.transform.parent.tag == "Portafilter") {
+        if (other.transform.parent != null && other.transform.parent.tag == "Portafilter") {
             float velocity = other.transform.parent.GetComponent<Rigidbody>().velocity.magnitude;
             if (velocity >= 4.0f) {
                 Portafilter portafilter = other.transform.parent.GetComponent<Portafilter>();
