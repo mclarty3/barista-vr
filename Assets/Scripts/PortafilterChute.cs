cab2043dcc2792f3038fa560703ae27581d5e5d3
mclarty3 <mclarty3@gmail.com>
2021-09-25 19:33:26 -0400

    Add check for if portafilter is facing down

--- Assets/Scripts/PortafilterChute.cs
+++ Assets/Scripts/PortafilterChute.cs
@@ -8,7 +8,8 @@ public class PortafilterChute : MonoBehaviour
     {
         if (other.transform.parent != null && other.transform.parent.tag == "Portafilter") {
             float velocity = other.transform.parent.GetComponent<Rigidbody>().velocity.magnitude;
-            if (velocity >= 4.0f) {
+            float angle = Vector3.Angle(other.transform.parent.transform.up, Vector3.up);
+            if (velocity >= 4.0f && angle > 120) {
                 Portafilter portafilter = other.transform.parent.GetComponent<Portafilter>();
                 portafilter.TrashEspresso();
             }
