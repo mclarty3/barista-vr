bac8094370f07897369ae2ee13990cb3950f9710
mclarty3 <mclarty3@gmail.com>
2021-09-26 05:59:04 -0400

    Implement alpha release gameplay

---
+++ Assets/Scripts/AlphaGameplayManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlphaGameplayManager : MonoBehaviour
+{
+
+    public Text orderText;
+    public Text timeText;
+    public Text performanceText;
+    public bool start = false;
+    public bool stop = false;
+
+    private float startTime;
+    private Beverage beverage;
+    private GameObject beverageObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StopRound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (start)
+        {
+            StartRound();
+            start = false;
+        } else if (stop) {
+            StopRound();
+            stop = false;
+        } else if (startTime != -1)
+        {
+            float timeElapsed = Time.time - startTime;
+            timeText.text = "Time elapsed: " + timeElapsed.ToString("00:00.000");
+        }
+    }
+
+    public void StartRound()
+    {
+        if (beverageObject != null)
+        {
+            Destroy(beverageObject);
+            beverageObject = null;
+        }
+        beverage = Beverage.GenerateBeverage();
+        orderText.text = "Order: " + beverage.name;
+        startTime = Time.time;
+        performanceText.text = "Place the crafted drink in the circle\nto see your performance";
+    }
+
+    public void StopRound()
+    {
+        if (beverageObject != null)
+        {
+            Destroy(beverageObject);
+            beverageObject = null;
+        }
+        orderText.text = "Order: No order yet";
+        timeText.text = "Time elapsed: 00:00.000";
+        performanceText.text = "Press start to receive\nan order and begin";
+        startTime = -1;
+    }
+
+    public void CompleteRound(ImprovedLiquid liquid)
+    {
+        beverageObject = liquid.gameObject;
+        beverage.DebugBeverageScore(liquid, out string message);
+        performanceText.text = message;
+        startTime = -1;
+    }
+}
---
+++ Assets/Scripts/DrinkDetector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkDetector : MonoBehaviour
+{
+
+    public AlphaGameplayManager agm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject otherObj = FindParentWithTag(other.gameObject, "Cup");
+        if (otherObj != null) {
+            ImprovedLiquid liquid = otherObj.GetComponentInChildren<ImprovedLiquid>();
+            if (liquid != null) {
+                agm.CompleteRound(liquid);
+            }
+        }
+    }
+
+    public static GameObject FindParentWithTag(GameObject childObject, string tag)
+    {
+        Transform t = childObject.transform;
+        while (t.parent != null)
+        {
+          if (t.parent.tag == tag)
+          {
+            return t.parent.gameObject;
+          }
+          t = t.parent.transform;
+        }
+        return null; // Could not find a parent with given tag.
+    }
+}
