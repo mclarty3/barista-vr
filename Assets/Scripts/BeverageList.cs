78aa4ae95a4d1e59d6fe062e3c8ca011dc449139
mclarty3 <mclarty3@gmail.com>
2021-09-26 03:31:49 -0400

    Various changes

---
+++ Assets/Scripts/BeverageList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeverageList 
+{
+    static public System.Func<float, float> PumpsToRatio = pumps => pumps * 0.02f;
+
+    static public List<Beverage> list = new List<Beverage>()
+    {
+        new Beverage("Latte", 150, new Dictionary<Ingredient.IngredientType, float>()
+            {
+                {Ingredient.IngredientType.Espresso, 0.25f},
+                {Ingredient.IngredientType.SteamedMilk, 0.75f},
+            }
+        ),
+        new Beverage("Steamed Milk", 145, new Dictionary<Ingredient.IngredientType, float>()
+            {
+                {Ingredient.IngredientType.Milk, 1}
+            }
+        ),
+        new Beverage("Cold Milk", 40, new Dictionary<Ingredient.IngredientType, float>()
+            {
+                {Ingredient.IngredientType.Milk, 1}
+            }
+        )
+    };
+}
---
+++ Assets/Scripts/ReturnToPlayArea.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToPlayArea : MonoBehaviour
+{
+    [Tooltip("The center of the play area; used for calculating distance")]
+    public Transform origin;
+    [Tooltip("The minimum distance the object must be from the origin before being respawned")]
+    public float distance = 7f;
+    [Tooltip("The time in seconds the object spends outside the zone before respawning")]
+    public float timeBeforeRespawn = 3f;
+    [Tooltip("The position and rotation the object will be respawned at")]
+    public Transform respawnPoint;
+    private Vector3 respawnPos;
+    private Quaternion respawnRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPos = respawnPoint.position;
+        respawnRot = respawnPoint.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, origin.position) > distance)
+        {
+            StartCoroutine(RespawnTimer());
+        }
+    }
+
+    IEnumerator RespawnTimer()
+    {
+        float time = 0f;
+        while (time < timeBeforeRespawn)
+        {
+            if (Vector3.Distance(transform.position, origin.position) <= distance) {
+                yield break;
+            }
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = respawnPos;
+        transform.rotation = respawnRot;
+    }
+}
