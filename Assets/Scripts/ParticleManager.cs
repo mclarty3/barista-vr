db4106f1436eaed1f6a2ba70b8a5178e82c73e65
mclarty3 <mclarty3@gmail.com>
2021-09-27 04:58:11 -0400

    Attempt to begin implementing new fluid rendering

---
+++ Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    public ParticleSystem particles;
+    public bool isPouring { 
+        get { return _isPouring;} 
+        set { 
+            Debug.Log("setting...");
+            _isPouring = value;
+            var em = particles.emission;
+            em.rateOverTime = value ? 2500 : 0;
+            Debug.Log(em.rateOverTime);
+        }
+    }
+    [SerializeField]
+    public bool _isPouring = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParticleSystem.ShapeModule shape = particles.shape;
+        shape.position = transform.position - particles.transform.position;
+        shape.rotation = transform.rotation.eulerAngles - particles.transform.rotation.eulerAngles;
+    }
+}
---
+++ Assets/Scripts/ParticleManagerEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ParticleManager))]
+public class ParticleManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Toggle pour"))
+        {
+            if (target.GetType() == typeof(ParticleManager))
+            {
+                ParticleManager pm = (ParticleManager)target;
+                pm.isPouring = !pm.isPouring;
+            }
+        }
+    }
+}
